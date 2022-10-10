47c3252bb759acf3ea4ccdd89d7dca7f9f250d2e
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-10-10 22:03:46 +0600

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './components/Main/Main';
+import Header from './components/Header/Header';
+import Quizes from './components/Quizes/Quizes';
+import Home from './components/Home/Home';
+
+const router = createBrowserRouter([
+  {path: '/', element: <Main></Main>, children: [
+    {path: '/home', element: <Home></Home>},
+    {path: '/home', element: <Header></Header>},
+
+    {path: '/quizes', 
+    loader:async() => {
+        return fetch('https://jsonplaceholder.typicode.com/users');
+    },
+    element: <Quizes></Quizes>},
+  ]},
+
+  {path: '/*', element: <div>not 404 found</div>},
+ ])
+
 
 function App() {
   return (
     <div className="App">
       <h3>OnlineQuiz.com</h3>
+      <RouterProvider router={router}></RouterProvider>
+
     </div>
   );
 }
