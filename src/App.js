8c3dcfa2d3249a29d2e49a98b9ed79ce069a20b3
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-10-10 22:28:28 +0600

    fevicon and name changed

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Main from './components/Main/Main';
 import Header from './components/Header/Header';
 import Quizes from './components/Quizes/Quizes';
 import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import Blog from './components/Blog/Blog';
 
 const router = createBrowserRouter([
   {path: '/', element: <Main></Main>, children: [
     {path: '/home', element: <Home></Home>},
-    {path: '/home', element: <Header></Header>},
+    {path: '/header', element: <Header></Header>},
+    {path: '/statistics', element: <Statistics></Statistics>},
+    {path: '/blog', element: <Blog></Blog>},
 
     {path: '/quizes', 
     loader:async() => {
@@ -25,7 +29,7 @@ const router = createBrowserRouter([
 function App() {
   return (
     <div className="App">
-      <h3>OnlineQuiz.com</h3>
+     
       <RouterProvider router={router}></RouterProvider>
 
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <Link to ='home'>Home</Link>
-            <Link to ='quizes'>Quizes</Link>
+        <div className='header'>
+            <div>
+                 <h3>OnlineQuiz.com</h3>
+            </div>
+
+            <div>
+                <Link to ='home'>Home</Link>
+                <Link to ='quizes'>QuizTopics</Link>
+                <Link to ='statistics'>Statistics</Link>
+                <Link to ='blog'>Blog</Link>
+            </div>
         </div>
     );
 };
