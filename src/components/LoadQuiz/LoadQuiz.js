e107324ead5e2b3406778f10a1326bfb1f387559
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-10-11 12:18:40 +0600

    Added radio buttob

--- src/components/LoadQuiz/LoadQuiz.js
+++ src/components/LoadQuiz/LoadQuiz.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './loadquiz.css'
 
 const LoadQuiz = (quizQuestion) => {
     console.log('qq= ', quizQuestion);
     return (
-        <div>
+        <div className='loadquiz'>
             <h4>question: {quizQuestion.quizQuestion.question}</h4>
-            <p> 1. {quizQuestion.quizQuestion.options[0]} 2. {quizQuestion.quizQuestion.options[1]} 3. {quizQuestion.quizQuestion.options[2]} 4. {quizQuestion.quizQuestion.options[3]} </p>
+
+            <input type="radio" value="1" name="gender" /> 1. {quizQuestion.quizQuestion.options[0]} <br/>
+            <input type="radio" value="2" name="gender" /> 2. {quizQuestion.quizQuestion.options[1]} <br/>
+            <input type="radio" value="3" name="gender" /> 3. {quizQuestion.quizQuestion.options[2]} <br/>
+            <input type="radio" value="4" name="gender" /> 4. {quizQuestion.quizQuestion.options[3]}
         </div>
     );
 };
